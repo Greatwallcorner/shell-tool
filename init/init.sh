1bd14e9c08310a471f12494e725617ea2fa3f964
heatdesert <wall.corner@outlook.com>
2022-12-01 12:09:13 +0800

    feat:安装卸载

--- init/init.sh
+++ init/init.sh
@@ -9,6 +9,11 @@ elif [[ $(command -v pacman) ]];then
     cmd='pacman'
 fi
 
+is_installed(){
+    echo "$1"
+    [ -z $(command -v $1) ] && $cmd install $1
+}
+
 
 _execute(){
     local currentDir=$(pwd)
@@ -24,10 +29,33 @@ installBaseTool(){
     npm config set registry http://registry.npmmirror.com
 }
 
+dir="/usr/lib/shell-tool"
+
+install_shell_tool(){
+    is_installed git
+    [ -d $dir ] && rm -rf $dir
+    mkdir -p $dir 
+    git clone -b main https://github.com/Greatwallcorner/shell-tool.git $dir --depth=1
+    chmod +x $dir/**
+    . $dir/init/init.sh
+}
+
+uninstall_shell_tool(){
+    rm -rf $dir
+    echo "删除目录:$dir"
+}
+
+# 安装目录 /usr/lib/shell-tool
+if [ ! -d /usr/lib/shell-tool ] || [ ! -d /usr/lib/shell-tool/init ];then
+    install_shell_tool
+    exit 
+fi
+[ ! -d /usr/lib/shell-tool/init ] && exit 1
 echo "------------"
 echo "1. 配置镜像源"
 echo "2. 配置zsh"
 echo "3. 安装基础工具"
+echo "4. 卸载本工具"
 read -p "请输入编号：" -d$'\n' num
 case $num in
 1) _execute config_pm_mirror.sh
@@ -35,6 +63,9 @@ case $num in
 2) _execute install_zsh.sh
 ;;
 3) installBaseTool
+;;
+4) uninstall_shell_tool
+;;
 esac
 
 
--- init/pmIs.sh
+++
@@ -1,7 +0,0 @@
-if [[ $(command -v yum) ]];then
-    cmd='yum'
-elif [[ $(command -v apt) ]];then
-    cmd='apt'
-elif [[ $(command -v pacman) ]];then
-    cmd='pacman'
-fi
